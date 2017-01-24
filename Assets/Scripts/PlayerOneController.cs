4fa50edb1390fb7929e6a1f653f600cc6eaffffd
Myeongzin Son <sonmyungzin1@naver.com>
2017-01-25 00:14:13 +0900

    create platform    add jump

--- Assets/Scripts/PlayerOneController.cs
+++ Assets/Scripts/PlayerOneController.cs
@@ -8,7 +8,7 @@ public class PlayerOneController : MonoBehaviour
     [System.NonSerialized] public float collideForce = 50f;
     [System.NonSerialized] public float gravity = 9.80f;
     private float maxSpeed = 5;
-    private bool isOnPlatform = true;
+    public bool isOnPlatform = false;
     private bool isFacingRight = false;
     Rigidbody2D rb2d;
 
@@ -19,10 +19,8 @@ public class PlayerOneController : MonoBehaviour
 
     void Update()
     {
-        if (!isOnPlatform)
-        {
-            rb2d.AddForce(new Vector2(0, -gravity));
-        }
+        if (Input.GetKeyDown("r"))
+        { transform.position = new Vector3(0f, 0f, transform.position.z); }
     }
     void FixedUpdate()
     {
@@ -37,22 +35,20 @@ public class PlayerOneController : MonoBehaviour
         if (Mathf.Abs(rb2d.velocity.x) <= maxSpeed)
         { rb2d.AddForce(new Vector2(inputDirection * moveForce, 0)); }
 
-        if (rb2d.velocity.x != 0)
-        { rb2d.AddForce(new Vector2(-1 * Mathf.Sign(rb2d.velocity.x) * collideForce, 0)); }
-
-        if (inputDirection == 0 && Mathf.Abs(rb2d.velocity.x) <= 0.2f)
+        if (inputDirection == 0 && Mathf.Abs(rb2d.velocity.x) <= 0.1f)
         { rb2d.velocity = new Vector2(0, rb2d.velocity.y); }
 
-        if (Input.GetKeyDown("z"))
+        if (Input.GetKeyDown("up"))
+        {
+            rb2d.AddForce(new Vector2(0, gravity*0.5f), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown("q"))
         { rb2d.AddForce(new Vector2(1000f, 0)); }
 
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Platform")
+        if (!isOnPlatform)
         {
-            isOnPlatform = true;
-            rb2d.velocity = new Vector2(0f, 0f);
+            rb2d.AddForce(new Vector2(0, -gravity));
         }
     }
 }
