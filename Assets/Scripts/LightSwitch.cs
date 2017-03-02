f0c4f81d75e4445a8e5313b729836c364c4f026a
Minjung Kim <Reny.atkins@gmail.com>
2017-03-02 09:51:36 +0900

    Make prefab - switch, lightbulb, light

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+
+	public GameObject Lightbulb;
+	bool isLightbulbOn;
+	public bool initStateOn;
+
+	public Sprite inactiveSprite;
+	public Sprite activeSprite;
+
+	// Use this for initialization
+	void Start () {
+		if (initStateOn)
+			TurnOnLight();
+		else
+			TurnOffLight();
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		Debug.Log("--");
+		if ((coll.gameObject.tag == "PlayerOne") || (coll.gameObject.tag == "PlayerTwo"))
+		{
+			if (isLightbulbOn)
+			{
+				TurnOffLight();
+			}
+			else
+			{
+				TurnOnLight();
+			}
+		}
+	}
+
+	void TurnOnLight()
+	{
+		Lightbulb.GetComponent<Lightbulb>().TurnOnLight();
+		GetComponent<SpriteRenderer>().sprite = activeSprite;
+		isLightbulbOn = true;
+	}
+
+	void TurnOffLight()
+	{
+		Lightbulb.GetComponent<Lightbulb>().TurnOffLight();
+		GetComponent<SpriteRenderer>().sprite = inactiveSprite;
+		isLightbulbOn = false;
+	}
+}
---
+++ Assets/Scripts/Lightbulb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightbulb : MonoBehaviour {
+
+	public GameObject lightObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TurnOnLight()
+	{
+		lightObject.SetActive(true);
+	}
+
+	public void TurnOffLight()
+	{
+		lightObject.SetActive(false);
+	}
+}
