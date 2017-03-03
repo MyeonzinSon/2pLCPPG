261d08646ae602798333261bd8e9afa91dbb9438
Myeongzin Son <sonmyungzin1@naver.com>
2017-03-04 04:07:33 +0900

    can rotate NeedleDispenser

--- Assets/Scripts/PoisonedNeedle.cs
+++ Assets/Scripts/PoisonedNeedle.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PoisonedNeedle : MonoBehaviour
 {
     public float speed;
+    float theta;
+    
     public void Initialize()
     {
         Destroy(gameObject);
     }
     void Start()
     {
+        
         StartCoroutine(Suicide());
     }
     IEnumerator Suicide()
@@ -20,8 +23,9 @@ public class PoisonedNeedle : MonoBehaviour
     }
     void FixedUpdate()
     {
+        theta = transform.rotation.eulerAngles.z*Mathf.PI/180;
         Vector3 position = transform.position;
-        transform.position = new Vector3(position.x + speed*Time.deltaTime, position.y, position.z);
+        transform.position = new Vector3(position.x + speed*Mathf.Cos(theta)*Time.deltaTime, position.y + speed*Mathf.Sin(theta)*Time.deltaTime, position.z);
     }
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -31,5 +35,12 @@ public class PoisonedNeedle : MonoBehaviour
             other.gameObject.GetComponent<PlayerTwoController>().Die();
         Destroy(gameObject);
     }
-
+    int Sign(float f)
+    {
+        if (f == 0)
+        { return 0; }
+        else if (f > 0)
+        { return 1; }
+        else { return -1; }
+    }
 }
