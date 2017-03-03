d2dfb75e92bb07fb5fad621e30e8103b2193bdbf
Myeongzin Son <sonmyungzin1@naver.com>
2017-03-04 04:36:31 +0900

    destroy PoisonedNeedle when collide with Platform

--- Assets/Scripts/PoisonedNeedle.cs
+++ Assets/Scripts/PoisonedNeedle.cs
@@ -6,14 +6,15 @@ public class PoisonedNeedle : MonoBehaviour
 {
     public float speed;
     float theta;
-    
+    bool born;
+
     public void Initialize()
     {
         Destroy(gameObject);
     }
     void Start()
     {
-        
+        born = false;
         StartCoroutine(Suicide());
     }
     IEnumerator Suicide()
@@ -23,18 +24,26 @@ public class PoisonedNeedle : MonoBehaviour
     }
     void FixedUpdate()
     {
-        theta = transform.rotation.eulerAngles.z*Mathf.PI/180;
+        theta = transform.rotation.eulerAngles.z * Mathf.PI / 180;
         Vector3 position = transform.position;
-        transform.position = new Vector3(position.x + speed*Mathf.Cos(theta)*Time.deltaTime, position.y + speed*Mathf.Sin(theta)*Time.deltaTime, position.z);
+        transform.position = new Vector3(position.x + speed * Mathf.Cos(theta) * Time.deltaTime, position.y + speed * Mathf.Sin(theta) * Time.deltaTime, position.z);
     }
     void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("hello");
         if (other.gameObject.tag == "PlayerOne")
             other.gameObject.GetComponent<PlayerOneController>().Die();
         if (other.gameObject.tag == "PlayerTwo")
             other.gameObject.GetComponent<PlayerTwoController>().Die();
-        Destroy(gameObject);
+        if (other.gameObject.tag == "Platform" && born)
+        { Destroy(gameObject); }
     }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+        { born = true; }
+    }
+
     int Sign(float f)
     {
         if (f == 0)
